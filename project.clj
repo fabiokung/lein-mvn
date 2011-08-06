8f093f868ba7e62cfa70790b1c65a06b8f540ec6
Fabio Kung <fabio.kung@gmail.com>
2011-08-06 03:10:38 -0300

    License and simple doc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-mvn "0.1.0"
-  :description "Lein plugin that serves as a bridge to Maven. It can be used to run any mvn goal on a maven-enabled project."
+  :description "Leiningen plugin that serves as a bridge to Maven. It can be used to run any mvn goal on a maven-enabled project."
   :dependencies
     [[org.clojure/clojure "1.2.1"]
      [org.clojure/clojure-contrib "1.2.0"]]
